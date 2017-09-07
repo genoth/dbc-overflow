b7ad0ff82a3dcba931595188d2384d6a18b91cac
Clint Thacker <couscous@gmail.com>
2017-09-07 17:36:01 -0500

    Fix error

--- app/controllers/question_controller.rb
+++ app/controllers/question_controller.rb
@@ -4,7 +4,6 @@ get '/questions/new' do
 end
 
 post '/questions' do
-  p params[:body]
   question = Question.create(title: params[:title], body: params[:body], user_id: 1)
   if question.save
     redirect "/questions/#{question.id}"
