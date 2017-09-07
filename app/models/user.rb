0f952967cc0f0b06e6ae4513d4ca7cfcd0c8a5f0
r3drighthand <ryanmccool13@gmail.com>
2017-09-07 16:11:15 -0500

    add bcrypt, authenticattion methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,22 @@ class User < ActiveRecord::Base
   has_many :votes, as: :votable
 
   validates :email, :username, {presence: true, uniqueness: true}
+
+  def password
+    @password ||= BCrypt::Password.new(self.password_hash)
+  end
+
+  def password=(input_password)
+    @password = BCrypt::Password.create(input_password)
+    self.password_hash = @password
+  end
+
+  def self.authenticate(email, password)
+    user = User.find_by(email: email)
+    if user && user.password == password
+      user
+    else
+      nil
+    end
+  end
 end
