e83a2ddddec298a5d3c68c2db22673c3f518e7a7
Gwynne Noth <nothgwynne@gmail.com>
2017-09-08 17:27:15 -0500

    Enable question voting without AJAX, Make dragon a little smaller for ease of testing and debugging

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,11 +1,10 @@
-get '/questions/:question_id/votes' do
+post '/questions/:question_id/votes' do
   @vote = Vote.new(value: params[:value].to_i, user_id: current_user.id, votable_type: "Question", votable_id: params[:question_id])
-  current_votes =
   if @vote.save
     if request.xhr?
-      # do the ajax thing
-    else
 
+    else
+      redirect :"/questions/#{params[:question_id]}"
     end
   else
     if request.xhr?
