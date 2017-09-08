8521f6008c4218426682ca6b9b57491300ea4082
Gwynne Noth <nothgwynne@gmail.com>
2017-09-08 18:49:56 -0500

    Continue working on voting logic.

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,5 +1,4 @@
 post '/questions/:question_id/votes' do
-  p params
   question = Question.find(params[:question_id])
   vote = Vote.new(value: params[:value].to_i, user: current_user, votable: question)
 
@@ -17,4 +16,47 @@ post '/questions/:question_id/votes' do
       #display errors with page
     end
   end
+end
+
+post '/questions/:question_id/comments/:comment.id' do
+  comment = Comment.find(params[:comment_id])
+  vote = Vote.new(value: params[:value].to_i, user: current_user, votable: comment)
+
+  if vote.save
+    if request.xhr?
+      content_type :json
+      {vote_count: comment.votes.sum(:value)}.to_json
+    else
+      redirect :"/questions/#{params[:question_id]}/comments/#{params[:comment_id]}"
+    end
+  else
+    if request.xhr?
+      #display errors with ajax
+    else
+      #redirect to page with errors
+    end
+  end
+end
+
+post '/answers/:answer_id/votes' do
+  answer = Answer.find(params[:answer_id])
+  vote = Vote.new(value: params[:value].to_i, user: current_user, votable: answer)
+
+  if vote.save
+    if request.xhr?
+      content_type :json
+      {vote_count: answer.votes.sum(:value)}.to_json
+    else
+      redirect :"/answer/#{params[:answer_id]}"
+    end
+  else
+    if request.xhr?
+      #display errors with ajax
+    else
+      #display errors with page
+    end
+  end
+end
+
+post '/answers/:answer_id/comments/:comment.id' do
 end
--- public/js/application.js
+++ public/js/application.js
@@ -27,7 +27,24 @@ var questionVoteListener = function(){
 }
 
 var answerVoteListener = function(){
+$("div.question_answers").on("submit", ".answer-vote", function(e){
+    e.preventDefault();
+    console.log("bound")
+    var $answerDiv = $("div.question_answers")
+    var $form = $(this);
+    var url = $form.attr('action');
 
+    var $request = $.ajax({
+      url: url,
+      method: "post",
+      dataType: "json",
+      data: $form.serialize()
+    })
+    $request.done(function(serverResponse){
+      var $voteSpan = $answerDiv.find(".vote-count")
+      $voteSpan.text(serverResponse.vote_count)
+    })
+  })
 }
 
 var qCommentVoteListener = function(){
