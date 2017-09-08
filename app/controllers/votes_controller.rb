84bd94b280960d2c6306ee3218c337c296d561a0
Gwynne Noth <nothgwynne@gmail.com>
2017-09-08 18:18:19 -0500

    Ajaxify voting logic

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,8 +1,12 @@
 post '/questions/:question_id/votes' do
-  @vote = Vote.new(value: params[:value].to_i, user_id: current_user.id, votable_type: "Question", votable_id: params[:question_id])
-  if @vote.save
-    if request.xhr?
+  p params
+  question = Question.find(params[:question_id])
+  vote = Vote.new(value: params[:value].to_i, user: current_user, votable: question)
 
+  if vote.save
+    if request.xhr?
+      content_type :json
+      {vote_count: question.votes.sum(:value)}.to_json
     else
       redirect :"/questions/#{params[:question_id]}"
     end
--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,39 @@
 $(document).ready(function() {
-  $
+  questionVoteListener();
+  answerVoteListener();
+  qCommentVoteListener();
+  aCommentVoteListener();
 });
+
+var questionVoteListener = function(){
+  $("div.question").on("submit", ".question-vote", function(e){
+    e.preventDefault();
+    console.log("bound")
+    var $questionDiv = $("div.question")
+    var $form = $(this);
+    var url = $form.attr('action');
+
+    var $request = $.ajax({
+      url: url,
+      method: "post",
+      dataType: "json",
+      data: $form.serialize()
+    })
+    $request.done(function(serverResponse){
+      var $voteSpan = $questionDiv.find(".vote-count")
+      $voteSpan.text(serverResponse.vote_count)
+    })
+  })
+}
+
+var answerVoteListener = function(){
+
+}
+
+var qCommentVoteListener = function(){
+
+}
+
+var aCommentVoteListener = function(){
+
+}
