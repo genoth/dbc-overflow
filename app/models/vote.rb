b2a72cad3d05a95f1ad17ee7c0384b5f772c1182
Gwynne Noth <nothgwynne@gmail.com>
2017-09-11 09:22:57 -0500

    Fix error partial

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,5 +2,6 @@ class Vote < ActiveRecord::Base
   belongs_to :user
   belongs_to :votable, polymorphic: true
 
+  validates :user_id, {presence: true}
   validates :user_id, uniqueness: {scope: :votable, message: "You may only vote once."}
 end
